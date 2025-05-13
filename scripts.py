3e8fd2a009b6d99de8e332b50b0cdc4a9a621d7e
OctaVianu8 <stanescu.matei.octavian@gmail.com>
2025-05-13 17:36:51 +0300

    merge cart-ul

--- scripts.py
+++ scripts.py
@@ -15,7 +15,7 @@ def update_password(username: str, new_password: str) -> bool:
             users = json.load(f)
         
         for user in users:
-            if user["user"] == username:
+            if user["username"] == username:
                 user["pass"] = hash_password(new_password)
                 break
         else:
@@ -26,4 +26,42 @@ def update_password(username: str, new_password: str) -> bool:
         return True
     except Exception as e:
         print(f"Error updating password: {e}")
+        return False
+    
+def add_to_cart_in_account(username: str, item: str):
+    try:
+        with open("users.json", "r") as f:
+            users = json.load(f)
+        for user in users:
+            if user["username"] == username:
+                user["cart"].append(item)
+                # print(user["cart"])
+                break
+        else:
+            return False  # User not found
+
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+        return True
+    except Exception as e:
+        print(f"Error updating cart: {e}")
+        return False
+    
+def remove_from_cart_in_account(username: str, item: str):
+    try:
+        with open("users.json", "r") as f:
+            users = json.load(f)
+        
+        for user in users:
+            if user["username"] == username:
+                user["cart"].remove(item)
+                break
+        else:
+            return False  # User not found
+
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+        return True
+    except Exception as e:
+        print(f"Error updating cart: {e}")
         return False
--- server.py
+++ server.py
@@ -12,11 +12,9 @@ def init_defaults():
     session.setdefault("authenticated", False)
     session.setdefault("user", {
         "username": "",
-        "email": "",
+        "cart": []
     })
     session.setdefault("error_msg", "")
-    session.setdefault("cart_count", 0)
-    session.setdefault("cart", [])
     session.setdefault("error_message", "")
     
 USERS_FILE = "users.json"
@@ -47,10 +45,11 @@ def login():
         error_msg = "Invalid username or password"
         
         for user in ALLOWED_USERS:
-            if user["user"] == username:
+            if user["username"] == username:
                 if scripts.check_password(password, user["pass"]):
                     session["authenticated"] = True
                     session["user"]["username"] = username
+                    session["user"]["cart"] = user["cart"]
                     error_msg = ""
                     return redirect("/")
         pass
@@ -70,34 +69,28 @@ def register():
             
         # Check if username already exists
         for user in ALLOWED_USERS:
-            if user["user"] == username:
+            if user["username"] == username:
                 error_msg = "Username already exists"
                 break
         else:
             # Add new user to the list
-            ALLOWED_USERS.append({"user": username, "pass": scripts.hash_password(password)})
+            ALLOWED_USERS.append({"username": username, "pass": scripts.hash_password(password), "cart": []})
             # Save updated list to users.json
             with open("users.json", "w") as f:
                 json.dump(ALLOWED_USERS, f)
             session["authenticated"] = True
-            session["username"] = username
+            
             return redirect("/")
     # If we reach here, it means registration failed
-    # (either due to invalid input or username already exists)
-    # Show the registration form again with an error message
-        
     return render_template("register.html", error_msg=error_msg, current_page="register")
 
 @app.route("/logout")
 def logout():
     session["authenticated"] = False
-    session["username"] = ""
     return redirect("/")
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
-    if not session.get("authenticated"):
-        return redirect("/login")
     
     error_msg = ""
     
@@ -128,37 +121,49 @@ def error404(code):
 
 @app.route("/cart/add/<product_id>", methods=["POST", "GET"])
 def add_to_cart(product_id):
-    if not session.get("authenticated"):
-        return redirect("/login")
-    session["cart"].append(product_id)
-    session["cart_count"] += 1
+    user = session["user"]
+    cart = session["user"]["cart"]
+    
+    if product_id in cart:
+        print("Product already in cart")
+        return redirect("/")
+    
+    scripts.add_to_cart_in_account(session["user"]["username"], product_id)
+    
+    cart.append(product_id)
+    user["cart"] = cart
+    session["user"] = user
+    
     return redirect("/")
 
 @app.route("/cart/remove/<product_id>", methods=["POST", "GET"])
 def remove_from_cart(product_id):
-    if not session.get("authenticated"):
-        return redirect("/login")
-    if product_id in session["cart"]:
-        session["cart"].remove(product_id)
-        session["cart_count"] -= 1
+    user = session["user"]
+    cart = session["user"]["cart"]
+    
+    if product_id in cart:
+        scripts.remove_from_cart_in_account(session["user"]["username"], product_id)
+        cart.remove(product_id)
+        user["cart"] = cart
+        session["user"] = user
     return redirect("/cart")
 
 @app.route("/cart")
 def cart():
-    if not session.get("authenticated"):
-        return redirect("/login")
     
     # Filter the cart items based on the product IDs
     cart_items = []
     with open("products.json", "r") as f:
         products = json.load(f)
-    for key, product in products.items():
-        if key in session["cart"]:
-            cart_items.append(product)
+    print(session["user"])
+    for id in session["user"]["cart"]:
+        if id in products:
+            cart_items.append(products[id])
+            
     # Calculate the total price of the cart items
     total_price = sum(item["price"] for item in cart_items)
     
-    return render_template("cart.html", cart_items=cart_items, current_page="cart")
+    return render_template("cart.html", cart_items=cart_items, current_page="cart", total_price=total_price)
 
 # Run the webserver (port 5000 - the default Flask port)
 if __name__ == "__main__":
